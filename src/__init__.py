33f18e0876a2c96f8101c5ac628fb0038e11d8c5
Dhullg <157047057+Dhullg@users.noreply.github.com>
2024-04-04 15:35:14 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ This module provides various functionalities for managing employee data.
 # Define the list of modules to be imported when the package is imported
 '''employee_operations: This module contains functions for performing various employee-related operations, such as adding, updating, or deleting employee data.
 file_operations: This module contains functions for handling file operations, such as reading and writing employee data to or from text files.
-menu: This module contains functions for displaying and handling user interactions with the system's menu to add.
+menu: This module contains functions for displaying and handling user interactions with the system's menu to add,delete and update employee data.
 report_generation: This module contains functions for generating various reports based on employee data.'''
 
 __all__ = ['employee_operations', 'file_operations', 'menu', 'report_generation']
