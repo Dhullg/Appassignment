13d88c2d06fd4023daec95ee0be4aa38b32f9205
Dhullg <157047057+Dhullg@users.noreply.github.com>
2024-04-04 15:34:01 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,11 @@ Employee Management System Module
 This module provides various functionalities for managing employee data.
 """
 # Define the list of modules to be imported when the package is imported
+'''employee_operations: This module contains functions for performing various employee-related operations, such as adding, updating, or deleting employee data.
+file_operations: This module contains functions for handling file operations, such as reading and writing employee data to or from text files.
+menu: This module contains functions for displaying and handling user interactions with the system's menu to add.
+report_generation: This module contains functions for generating various reports based on employee data.'''
+
 __all__ = ['employee_operations', 'file_operations', 'menu', 'report_generation']
 
 # Import modules to make them available when the package is imported
